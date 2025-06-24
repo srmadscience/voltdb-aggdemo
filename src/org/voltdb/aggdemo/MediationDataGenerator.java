139ede3f86f5b2fad9b843e58cd120c51b3f93ba
dwrolfe <david.rolfe@pobix.com>
2025-06-24 16:29:36 +0100

    Producer options

--- src/org/voltdb/aggdemo/MediationDataGenerator.java
+++ src/org/voltdb/aggdemo/MediationDataGenerator.java
@@ -656,7 +656,8 @@ public class MediationDataGenerator {
 
         Properties props = new Properties();
         props.put("bootstrap.servers", kafkaBrokers.toString());
-        props.put("acks", "1");
+        props.put("bootstrap.servers.voltdb", commaDelimitedHostnames);
+        props.put("acks", "all");
         props.put("retries", 0);
 
         // batch.size is how many bytes a batch can take up, not how many records in a
